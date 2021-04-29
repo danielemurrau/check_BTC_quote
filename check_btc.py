07c8648fc6dd9dbadef69857826b233c558651dd
root <root@LAPTOP-S3V8FVC9.localdomain>
2021-04-29 10:35:37 +0200

    commit code

---
+++ check_btc.py
@@ -0,0 +1,31 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+import time
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+          'start':'1',
+            'limit':'1',
+              'convert':'EUR'
+              }
+headers = {
+          'Accepts': 'application/json',
+            'X-CMC_PRO_API_KEY': 'INSERT HERE YOUR COINMARKETCAP API KEY',
+            }
+
+session = Session()
+session.headers.update(headers)
+while True:
+    try:
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        print ("----------------------------------------------")
+        print (data['status']['timestamp'] + " ", end = '')
+        print (data['data'][0]['name'] + " ", end = '')
+        print (data['data'][0]['symbol'] + " ", end = '')
+        print (data['data'][0]['quote']['EUR']['price'])
+        print ("----------------------------------------------")
+        time.sleep(30)
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)
